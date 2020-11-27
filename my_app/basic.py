4f5a47e7a4cd213df77c0242f2534639498f0be9
Your Name <test@example.com>
2020-11-27 09:04:05 +0100

    update cors

--- my_app/basic.py
+++ my_app/basic.py
@@ -1,6 +1,15 @@
 from flask import Flask, send_file, render_template, Blueprint, redirect
+from flask_cors import CORS
+
+
+
 
 bApp = Blueprint('bApp', __name__)
+
+
+cors = CORS(bApp, resources={r"/*": {"origins": "https://mayankfawkes.xyz"}})
+
+
 
 @bApp.route('/')
 def home():
--- my_app/errors.py
+++ my_app/errors.py
@@ -1,6 +1,11 @@
 from flask import Flask, send_file, render_template, Blueprint, redirect
+from flask_cors import CORS
+
+
+
 bp = Blueprint('errors', __name__)
 
+cors = CORS(bp, resources={r"/*": {"origins": "https://mayankfawkes.xyz"}})
 
 @bp.app_errorhandler(Exception)
 def handle_error(e):
